181357e08d3efe0ff401feb9eeb550e23273f8f1
Vishal Bhagwan Gadge <vishalgadge2005@gmail.com>
2026-07-29 15:30:28 +0530

    migrate from railway to tidb cloud and 5 min github action workflow every 5 min

--- src/main/java/com/danger/will/MedLink/api/controller/DoctorController.java
+++ src/main/java/com/danger/will/MedLink/api/controller/DoctorController.java
@@ -2,6 +2,7 @@ package com.danger.will.MedLink.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,8 @@ import com.danger.will.MedLink.api.service.services.DoctorService;
 @RestController
 @RequestMapping("/api/doctor")
 public class DoctorController {
-    
+
+    @Autowired
     DoctorService doctorService;
 
     DoctorController(DoctorServiceImpl doctorServiceImpl){
